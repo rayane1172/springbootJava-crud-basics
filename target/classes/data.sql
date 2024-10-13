7d35e48ad42d25f46ebb37d6e4613dfc55aa4a7e
rayane-bouchair <bouchairrayane01@gmail.com>
2024-10-13 01:32:44 +0100

    all necessary files

---
+++ target/classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (fname,sname, salary) VALUES ('rayane','Bouchair',200);
+INSERT INTO employee (fname,sname, salary) VALUES ('zaki','Catalonia',300);
+INSERT INTO employee (fname,sname, salary) VALUES ('mohammed','Lacolombe',150);
+INSERT INTO employee (fname,sname, salary) VALUES ('Maikel','Hendelston',150);
+INSERT INTO employee (fname,sname, salary) VALUES ('diego','costa',150);
+INSERT INTO employee (fname,sname, salary) VALUES ('Brono','trader',150);
+
